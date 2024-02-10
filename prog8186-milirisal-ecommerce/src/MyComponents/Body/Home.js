b4c5ca5560704ad65783689cb77a4eca66f1238f
MiliRisal <milirisal11@gmail.com>
2024-02-10 17:07:36 -0500

    Products listed statically

--- prog8186-milirisal-ecommerce/src/MyComponents/Body/Home.js
+++ prog8186-milirisal-ecommerce/src/MyComponents/Body/Home.js
@@ -1,7 +1,32 @@
-import React from "react";
-
+import { Container } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import tshirt from "../Body/tshirt.jpg";
 function Home() {
-  return <h1>This is home page</h1>;
+  return (
+    <Container className="m-5">
+      <Row xs={1} md={3} className="g-4">
+        {Array.from({ length: 9 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Img variant="top" src={tshirt} />
+              <Card.Body>
+                <Card.Title>Cagdyuisuydfu</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting t ext below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Button variant="primary">Add to cart</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
--- prog8186-milirisal-ecommerce/src/MyComponents/Footer.js
+++ prog8186-milirisal-ecommerce/src/MyComponents/Footer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Footer() {
-  return <h1>This is footer</h1>;
+  return (
+    <Card className="text-center">
+      <Card.Body>
+        <Card.Title>Shop More</Card.Title>
+        <Card.Text>Blog</Card.Text>
+        <Card.Text>FAQs</Card.Text>
+        <Card.Text>Contact us</Card.Text>
+        <Card.Text>Follow us</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">
+        © Copyright 2012 - 2024 | Shop More by Mili Risal | All rights reserved
+      </Card.Footer>
+    </Card>
+  );
 }
 
 export default Footer;
